e41f59bfc22007e9581fdbb185a14185f26608c3
Anna Rotkiel-Kozik <anna.rotkiel@wp.pl>
2021-11-06 10:03:09 +0100

    Change js

--- js/functions.js
+++ js/functions.js
@@ -8,8 +8,4 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = 'kamień';
-let playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeżeli Twój ruch to ' + playerMove + ', to wygrywasz!');
 
